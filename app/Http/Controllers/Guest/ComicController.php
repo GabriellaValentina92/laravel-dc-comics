6902ad241b0f302ad1867ed37406628cc009d548
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-06-30 15:52:00 +0200

    Add form new information saved

--- app/Http/Controllers/Guest/ComicController.php
+++ app/Http/Controllers/Guest/ComicController.php
@@ -37,7 +37,37 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validare i dati 
+        $request ->validate([
+            'title'=> 'required|string|max:100',
+            'thumb'=> 'required|url',
+            'description' => 'required|string',
+            'series' => 'required|string|max:50',
+            'type' => 'required|string|max:30',
+            'sale_date'=> 'required|date',
+            'price' => 'required|decimal:2',
+            
+        ]);
+
+        $data = $request->all();
+        // salvare i dati nel db (questo metodo anche se è più lungo è il più sicuro)
+        $newComic = new Comic();
+        $newComic->title = $data['title'];
+        $newComic->thumb = $data['thumb'];
+        $newComic->description = $data['description'];
+        $newComic->series = $data['series'];
+        $newComic->type = $data['type'];
+        $newComic->sale_date = $data['sale_date'];
+        $newComic->price = $data['price'];
+        $newComic-> save();
+
+        //serve per togliere il token per metterlo dentro l'array se lo facciamo in automatico e non come sopra
+        unset($data['_token']);
+        return redirect()-> route('comics.show', ['comic'=> $newComic->id]);
+
+        //metodo 2 
+        //Comic::create($data);
+
     }
 
     /**
--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
     use HasFactory;
+
+    //vengono compilati solo i campi inseriti nel fillable il resto viene lasciato null ma nn da errore (se vogliamo fare il metodo corto)
+    //protected $fillable = ['title', 'description', 'series', 'type', 'price']; 
 }
