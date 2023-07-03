1fac2862a219a4603016999f6d1eb0388d8b652c
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-07-03 17:12:32 +0200

    Add basic delete/404 error

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,19 +1,15 @@
-@extends('errors::minimal')
-
-@section('title', __('Not Found'))
-@section('code', '404')
+@extends('layout.base')
 
 
-{{-- @section('message')
+@section('content_comic')
 <div class="container-error">
-    <img src="{{$comic->thumb[1]}}" alt="">
     <h2>Ooooops!</h2>
     <p>hei, fumettaro...siamo spiacenti ma sei finito nella pagina sbagliata!
         clicca nel bottone in basso e a velocità supersonica ti reindirizzeremo nella nostra pagina principale.
     </p>
     <a href="{{ route('comics.index')}}"> per i Superpoteri da questa parte</a>
 </div>
-@endsection --}}
+@endsection
 
 
 
